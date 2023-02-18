9333cc05c89dc3733ed6fd0d550b1e70664da42c
Webmaster1K <volosienko9@gmail.com>
2023-02-18 18:55:55 +0300

    new file:   public/img/addButton.svg 	new file:   public/img/clickedButton.svg 	modified:   src/components/main/Main.css 	modified:   src/components/main/Main.js 	modified:   "src/components/main/card/\320\241ard.js"

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,11 +1,29 @@
 import './Main.css';
 import Сard from './card/Сard';
+import Stack from 'react-bootstrap/Stack';
 
 function Main() {
   return (
     <>
       <main className='main'>
-        <Сard/>
+        <Stack direction="horizontal" gap={3}>
+            <h1>Все чаи</h1>
+            <input className="ms-auto"/>
+        </Stack>
+        <div className='allCard'>
+          <Сard/>
+          <Сard/>
+          <Сard/>
+          <Сard/>
+          <Сard/>
+          <Сard/>
+          <Сard/>
+          <Сard/>
+          <Сard/>
+          <Сard/>
+          <Сard/>
+          <Сard/>
+        </div>
       </main>
     </>
   );
--- src/components/main/card/Сard.js
+++ src/components/main/card/Сard.js
@@ -1,20 +1,22 @@
 import './Сard.css';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Stack from 'react-bootstrap/Stack';
+import { useState } from 'react';
 
 function Сard() {
+  const [addedCart, SetAddedCart] = useState(false);
+
   return (
-    <Card style={{ width: '210px', height: '260px' }}>
-      <Card.Img variant="top" src="/img/watch1.png" style={{ width: '123', height: '123px' }} />
+    <Card style={{width: '210px', margin: "25px 0"}}>
+      <Card.Img variant="top" src="/img/watch1.png" style={{ width: '123', height: '123' }} />
       <Card.Body>
         <Card.Title className='cardTitle'>Часы Бронницкий Ювелир</Card.Title>
-        <Card.Text style={{fontSize: 14}}>
-          Цена:
-        </Card.Text>
-        <Card.Text>
-          <b>54 590 ₽</b>
-          <Button variant="outline-success">+</Button>
-        </Card.Text>
+        <Stack direction="horizontal" gap={3}>
+          <div><span style={{fontSize: "11px", color: '#BDBDBD'}}>Цена:</span><br/> 54 590 ₽</div>
+          <div className="ms-auto">
+            <img src={addedCart ? './img/clickedButton.svg' : './img/addButton.svg'} onClick={() => SetAddedCart(!addedCart)}/>
+          </div>
+        </Stack>
       </Card.Body>
     </Card>
   );
