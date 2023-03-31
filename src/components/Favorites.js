3b0b75229499ec78f4bbb4976adf24ba5f250765
Webmaster1K <volosienko9@gmail.com>
2023-03-31 21:24:19 +0300

    modified:   src/components/Favorites.js 	modified:   src/components/main/Main.js 	modified:   "src/components/main/card/\320\241ard.js"

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Stack from 'react-bootstrap/Stack'
 import {Row, Col} from 'react-bootstrap'
 import CartOne from './cart/cartOne/CartOne';
@@ -22,25 +21,4 @@ return <CartOne name={props.name} cont={props.cont} img={props.img} />})
     );
   }
   
-=======
-import Stack from 'react-bootstrap/Stack'
-import {Row, Col} from 'react-bootstrap'
-
-function Favorites() {
-    return (
-      <>
-        <div style={{maxWidth: "1080px", margin: "0 auto 0 auto", backgroundColor: "white", borderBottom: "1px solid #EAEAEA", padding: "45px 45px"}}>
-          <Stack direction="horizontal" gap={3}>
-              <h1>Избранные</h1>
-          </Stack>
-          <Row className='g-4'>
-              <Col style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around'}}>
-              </Col>
-          </Row>
-        </div>
-      </>
-    );
-  }
-  
->>>>>>> a06d1a9856ba4e526f9c35cfce7b3fe9ae8d0a02
 export default Favorites;
